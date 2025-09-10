7756eee65dcebc3fb170136856a1a6fdaf613fac
anderson <anderson@anderson>
2025-09-10 14:26:38 -0300

    criei seções nos dados do Hero

--- heroes/admin.py
+++ heroes/admin.py
@@ -4,6 +4,19 @@ from .models import Hero
 # Register your models here.
 @admin.register(Hero)
 class HeroAdmin(admin.ModelAdmin):
-    list_display = ['codinome', 'nome_real', 'poder_principal', 'cidade', 'criado_em']
-    list_filter = ['cidade']
-    search_fields = ['codinome', 'nome_real', 'cidade']
+    list_display = ['codinome', 'nome_real', 'poder_principal', 'cidade', 'criado_em'] # campos exibidos na listagem
+    list_filter = ['cidade'] # campo disponivel para filtrar os dados
+    search_fields = ['codinome', 'nome_real', 'cidade']  # campos a serem pesquisados na busca
+
+    fieldsets = (   # divide em seções
+        ('Identidade Secreta', {
+            'fields': ('codinome', 'nome_real')
+        }),
+        ('Informações Gerais', {
+            'fields': ('poder_principal', 'cidade', 'historia')
+        }),
+        ('Dados de Registro', {
+            'fields': ('criado_em',)
+        }),
+    )
+    readonly_fields = ['criado_em']    
