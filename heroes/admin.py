6d6ac9bc06f150f5767bc83ad9e345e2dc0557b8
anderson <anderson@anderson>
2025-09-10 14:23:39 -0300

    personalizei exibição de lista e busca no Hero no Admin interface

--- heroes/admin.py
+++ heroes/admin.py
@@ -2,4 +2,8 @@ from django.contrib import admin
 from .models import Hero
 
 # Register your models here.
-admin.site.register(Hero)
+@admin.register(Hero)
+class HeroAdmin(admin.ModelAdmin):
+    list_display = ['codinome', 'nome_real', 'poder_principal', 'cidade', 'criado_em']
+    list_filter = ['cidade']
+    search_fields = ['codinome', 'nome_real', 'cidade']
