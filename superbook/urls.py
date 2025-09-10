8c85efdcf358214ed0c35dc7ead60cd2fc1a40b4
anderson <anderson@anderson>
2025-09-10 14:36:34 -0300

    personalizei alguns titulos da area de admin

--- superbook/urls.py
+++ superbook/urls.py
@@ -16,6 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib import admin
+
+admin.site.site_header = "SuperBook Admin"
+admin.site.site_title = "SuperBook Painel"
+admin.site.index_title = "Bem-vindo ao SuperBook"
 
 urlpatterns = [
     path('admin/', admin.site.urls),
