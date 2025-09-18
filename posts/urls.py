11da2af226fe8f3f4271374f9498b7eeca69fa3c
anderson <anderson@anderson>
2025-09-18 14:14:51 -0300

    criado rota do detalhe_post

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostCreateView, PostUpdateView, PostDeleteView, detalhe_post
 
 urlpatterns = [    
     
@@ -8,5 +8,6 @@ urlpatterns = [
     path('novo/', PostCreateView.as_view(), name='novo_post'), 
     path('<int:pk>/editar/', PostUpdateView.as_view(), name='editar_post'),   
     path('<int:pk>/excluir/', PostDeleteView.as_view(), name='excluir_post'),
+    path('<int:pk>/', detalhe_post, name='detalhe_post'),
 ]
 
