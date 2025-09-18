37dbdadc76c9b3ee96c10f373ef755b373a2bc5b
anderson <anderson@anderson>
2025-09-18 15:20:37 -0300

    implementei melhoias visuais no lista_posts

--- heroes/models.py
+++ heroes/models.py
@@ -9,6 +9,7 @@ class Hero(models.Model):
     historia = models.TextField(blank=True, null=True)
     criado_em = models.DateTimeField(auto_now_add=True)
     email_contato = models.EmailField(blank=True, null=True)
+    imagem = models.ImageField(upload_to='fotos_herois/', blank=True, null=True)
     
 
     def __str__(self):
