c784cda76e775537e9ce5368b0284e81d2c6c282
anderson <anderson@anderson>
2025-10-17 14:58:30 -0300

    implementado autenticação nos templates, incluindo socialaccount

--- posts/views.py
+++ posts/views.py
@@ -1,37 +1,71 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from django.views.generic import ListView
-from .models import Post
-from .forms import PostForm
-from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
+from .models import Post
+from heroes.models import Hero
+from .forms import PostForm
 from comments.forms import ComentarioForm
-
 
 class PostListView(ListView):
     model = Post
-    template_name = "posts/lista_posts.html"
-    context_object_name = "posts"
-    
-class PostCreateView(CreateView):
-    model = Post
-    form_class = PostForm
-    template_name = 'posts/form_post.html'
-    success_url = reverse_lazy('lista_posts') 
-    
-class PostUpdateView(UpdateView):
+    template_name = 'posts/lista_posts.html'
+    context_object_name = 'posts'
+    ordering = ['-criado_em']
+
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     form_class = PostForm
     template_name = 'posts/form_post.html'
     success_url = reverse_lazy('lista_posts')
-    
-class PostDeleteView(DeleteView):
+
+    def form_valid(self, form):
+        try:
+            hero = self.request.user.hero
+        except Hero.DoesNotExist:
+            messages.error(self.request, "Crie seu perfil de herói antes de publicar.")
+            return redirect('criar_heroi')
+        form.instance.autor = hero
+        messages.success(self.request, "Post criado com sucesso.")
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    form_class = PostForm
+    template_name = 'posts/form_post.html'
+    success_url = reverse_lazy('lista_posts')
+
+    def test_func(self):
+        obj = self.get_object()
+        try:
+            return self.request.user.is_authenticated and obj.autor == self.request.user.hero
+        except Hero.DoesNotExist:
+            return False
+
+    def handle_no_permission(self):
+        messages.error(self.request, "Você não tem permissão para editar este post.")
+        return redirect('lista_posts')
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'posts/confirmar_exclusao.html'
-    success_url = reverse_lazy('lista_posts')    
+    success_url = reverse_lazy('lista_posts')
+
+    def test_func(self):
+        obj = self.get_object()
+        try:
+            return self.request.user.is_authenticated and obj.autor == self.request.user.hero
+        except Hero.DoesNotExist:
+            return False
+
+    def handle_no_permission(self):
+        messages.error(self.request, "Você não tem permissão para excluir este post.")
+        return redirect('lista_posts')
 
 def detalhe_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    comentarios = post.comentarios.all()
+    comentarios = post.comentarios.all().order_by('criado_em') if hasattr(post, 'comentarios') else []
 
     if request.method == 'POST':
         form = ComentarioForm(request.POST)
@@ -39,6 +73,7 @@ def detalhe_post(request, pk):
             comentario = form.save(commit=False)
             comentario.post = post
             comentario.save()
+            messages.success(request, "Comentário adicionado.")
             return redirect('detalhe_post', pk=post.pk)
     else:
         form = ComentarioForm()
@@ -47,4 +82,4 @@ def detalhe_post(request, pk):
         'post': post,
         'comentarios': comentarios,
         'form': form
-    })    
+    })
--- superbook/urls.py
+++ superbook/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
+from heroes.views import redirect_after_login
 
 admin.site.site_header = "SuperBook Admin"
 admin.site.site_title = "SuperBook Painel"
@@ -29,9 +30,12 @@ urlpatterns = [
     path('heroes/', include('heroes.urls')),  # rotas do app heroes
     path('posts/', include('posts.urls')),    # rotas do app posts
     path('villains/', include('villains.urls')),
-    path('accounts/', include('allauth.urls')),  # login/logout/signup + Google
-]
 
+# Autenticação (allauth)
+    path('accounts/', include('allauth.urls')),
+    path("redirect/", redirect_after_login, name="redirect_after_login"),
+    
+]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
